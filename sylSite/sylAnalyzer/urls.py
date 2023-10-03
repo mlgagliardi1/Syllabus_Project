2a26cc19022f64c7848a08446b523c6e60c12d06
Michael Gagliardi <124767275+mlgagliardi1@users.noreply.github.com>
2023-10-03 17:55:35 -0500

    Template Framework Created
    Added homepage, examples, details HTML, CSS, JS. Added urls to sylAnalyzer url config.

--- sylSite/sylAnalyzer/urls.py
+++ sylSite/sylAnalyzer/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "sylAnalyzer"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("results/", views.results, name="results"),
+    path("details/", views.details, name="details"),
+    path("examples/", views.examples, name="examples"),
 ]
--- sylSite/sylAnalyzer/views.py
+++ sylSite/sylAnalyzer/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("Hello World, you are at the syllabus analyzer homepage.")
+    return render(request, "sylAnalyzer/homepage.html")
 
-def results(request):
-    return HttpResponse("Hello again, you are at the results page.")
+def details(request):
+    return render(request, "sylAnalyzer/details.html")
+
+def examples(request):
+    return render(request, "sylAnalyzer/examples.html")
