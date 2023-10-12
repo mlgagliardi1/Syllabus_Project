6e3cf3f3b9c434ecffd61d2563323aab1df9025c
Michael Gagliardi <124767275+mlgagliardi1@users.noreply.github.com>
2023-10-12 13:46:45 -0500

    Handled erroneous uploads
    Handled erroneous uploads by creating an uploaderror view which alerts the user (JS) and accepts a new file. This loops until an accepted file is input.

--- sylSite/sylAnalyzer/urls.py
+++ sylSite/sylAnalyzer/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "sylAnalyzer"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("uploaderror/", views.uploaderror, name="uploaderror"),
     path("uploaded/", views.uploaded, name="uploaded"),
+    path("uploaderror/", views.uploaderror, name="uploaderror"),
     path("results/", views.results, name="results"),
     path("examples/", views.examples, name="examples"),
     path("explanation/", views.explanation, name="explanation"),
--- sylSite/sylAnalyzer/views.py
+++ sylSite/sylAnalyzer/views.py
@@ -4,14 +4,20 @@ from django.urls import reverse
 from .forms import UploadFileForm
 
 # Create your views here.
-
 def homepage(request):
-    uploadFileForm = {}
-    uploadFileForm['form'] = UploadFileForm()
-    return render(request, "sylAnalyzer/base_homepage.html", uploadFileForm)
+    uploadFileForm = UploadFileForm()
+    context = {
+        "upload_form": uploadFileForm,
+    }
+    return render(request, "sylAnalyzer/base_homepage.html", context)
 
 def uploaderror(request):
-    return render(request, "sylAnalyzer/base_homepage.html")
+    uploadFileForm = UploadFileForm()
+    context = {
+        "upload_form": uploadFileForm,
+        "uploaded": False,
+    }
+    return render(request, "sylAnalyzer/base_homepage.html", context)
 
 def results(request):
     return render(request, "sylAnalyzer/base_details.html")
@@ -25,11 +31,11 @@ def explanation(request):
 def uploaded(request):
     #Create the form using models and forms.py
     form = UploadFileForm(request.POST, request.FILES)
-    #Check the request is a POST and contains a file
+
+    #Check the request is a POST, contains a file, and the form is valid
     if request.method == "POST" and request.FILES['file'] and form.is_valid():
-        #Create the uploadedFile var and pass it to saveFile. If file saved properly, redirect to results, else redirect to homepage
         form.save()
         return redirect("/syllabusanalyzer/results")
-    else:
-        form = UploadFileForm()
-        return redirect("/syllabusanalyzer/uploaderror")
+    
+    else: #form is not valid
+        return redirect("/syllabusanalyzer/uploaderror/")
